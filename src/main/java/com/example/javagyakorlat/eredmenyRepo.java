93e373c3f87a36d018410e8d7a2d7c703338ac05
Tóth Csaba <lagolas420@gmail.com>
2025-11-29 13:05:11 +0100

    Setup for upload

--- src/main/java/com/example/javagyakorlat/eredmenyRepo.java
+++ src/main/java/com/example/javagyakorlat/eredmenyRepo.java
@@ -1,12 +1,7 @@
 package com.example.javagyakorlat;
 
-import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.sql.Date;
 import java.util.List;
 
 public interface eredmenyRepo extends CrudRepository<eredmeny,Integer> {
